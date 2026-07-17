6272061cfbae4a3ec8bb73bc40e128a73ca778ef
diputs-sudo <diputs-sudo@proton.me>
2026-07-17 16:33:48 -0400

    add bubble.ts

---
+++ src/sorting/algorithms/bubble.ts
@@ -0,0 +1,42 @@
+import { Step } from "../visualizer/types";
+
+export function* bubbleSort(input: number[]): Generator<Step> {
+    const arr = [...input];
+
+    const n = arr.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+
+            yield {
+                type: "compare",
+                indices: [j, j + 1],
+                array: [...arr],
+                memoryUsed: 0,
+                memoryLabel: "Memory",
+                memoryUnit: "items"
+            };
+
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+
+                yield {
+                    type: "swap",
+                    indices: [j, j + 1],
+                    array: [...arr],
+                    memoryUsed: 0,
+                    memoryLabel: "Memory",
+                    memoryUnit: "items"
+                };
+            }
+        }
+    }
+
+    yield {
+        type: "done",
+        array: [...arr],
+        memoryUsed: 0,
+        memoryLabel: "Memory",
+        memoryUnit: "items"
+    };
+}
