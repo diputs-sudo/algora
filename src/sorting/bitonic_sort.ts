2bb477220213544f89004f60f78ee1e7a6d86cef
diputs-sudo <diputs-sudo@proton.me>
2026-07-28 21:55:11 -0400

    update bitonic_sort.ts

--- src/sorting/bitonic_sort.ts
+++ src/sorting/bitonic_sort.ts
@@ -37,6 +37,10 @@ document.addEventListener("DOMContentLoaded", () => {
         defaultLanguage: "python",
         moreInfoSelector: "#standardMoreInfo"
     });
+    initCodeLoader("sorting", "bitonic_sort_production", {
+        rootSelector: "#productionCodeSection",
+        defaultLanguage: "python"
+    });
 
     const graph = new Graph("graphContainer");
 
