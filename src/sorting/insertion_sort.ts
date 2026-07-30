0463eab924e6d9736636600da2d697441befa12f
diputs-sudo <diputs-sudo@proton.me>
2026-07-30 17:43:25 -0400

    update insertion_sort.ts

--- src/sorting/insertion_sort.ts
+++ src/sorting/insertion_sort.ts
@@ -12,6 +12,10 @@ document.addEventListener("DOMContentLoaded", () => {
         defaultLanguage: "python",
         moreInfoSelector: "#standardMoreInfo"
     });
+    initCodeLoader("sorting", "insertion_sort_production", {
+        rootSelector: "#productionCodeSection",
+        defaultLanguage: "python"
+    });
 
     const graph = new Graph("graphContainer");
 
