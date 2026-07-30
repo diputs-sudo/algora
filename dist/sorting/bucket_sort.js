72d04976e203bec22b5db34e2b83beb10377a3bc
diputs-sudo <diputs-sudo@proton.me>
2026-07-29 23:16:48 -0400

    update bucket_sort.js

--- dist/sorting/bucket_sort.js
+++ dist/sorting/bucket_sort.js
@@ -38,6 +38,10 @@ document.addEventListener("DOMContentLoaded", () => {
         defaultLanguage: "python",
         moreInfoSelector: "#standardMoreInfo"
     });
+    initCodeLoader("sorting", "bucket_sort_production", {
+        rootSelector: "#productionCodeSection",
+        defaultLanguage: "python"
+    });
     const graph = new Graph("graphContainer");
     let dataset = generateRandomArray(20);
     let generator = bucketSort(dataset);
