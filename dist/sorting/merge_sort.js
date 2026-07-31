5c79f2ff289be9a6347467b7d9627aa54e206a15
diputs-sudo <diputs-sudo@proton.me>
2026-07-30 21:36:30 -0400

    update merge_sort.js

--- dist/sorting/merge_sort.js
+++ dist/sorting/merge_sort.js
@@ -22,6 +22,10 @@ document.addEventListener("DOMContentLoaded", () => {
         defaultLanguage: "python",
         moreInfoSelector: "#standardMoreInfo"
     });
+    initCodeLoader("sorting", "merge_sort_production", {
+        rootSelector: "#productionCodeSection",
+        defaultLanguage: "python"
+    });
     const graph = new Graph("graphContainer");
     let dataset = generateRandomArray(20);
     let generator = mergeSort(dataset);
