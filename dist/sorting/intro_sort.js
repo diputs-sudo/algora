d29a6410e24049a0eeed22da48c2173d620df720
diputs-sudo <diputs-sudo@proton.me>
2026-07-30 21:36:30 -0400

    update intro_sort.js

--- dist/sorting/intro_sort.js
+++ dist/sorting/intro_sort.js
@@ -24,6 +24,10 @@ document.addEventListener("DOMContentLoaded", () => {
         defaultLanguage: "python",
         moreInfoSelector: "#standardMoreInfo"
     });
+    initCodeLoader("sorting", "intro_sort_production", {
+        rootSelector: "#productionCodeSection",
+        defaultLanguage: "python"
+    });
     const graph = new Graph("graphContainer");
     let dataset = generateRandomArray(20);
     let generator = introSort(dataset);
