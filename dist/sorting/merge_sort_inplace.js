0abd2f116578ba3ac22581aead4f05742200f557
diputs-sudo <diputs-sudo@proton.me>
2026-07-31 14:40:27 -0400

    update merge_sort_inplace.js

--- dist/sorting/merge_sort_inplace.js
+++ dist/sorting/merge_sort_inplace.js
@@ -22,6 +22,10 @@ document.addEventListener("DOMContentLoaded", () => {
         defaultLanguage: "python",
         moreInfoSelector: "#standardMoreInfo"
     });
+    initCodeLoader("sorting", "merge_sort_inplace_production", {
+        rootSelector: "#productionCodeSection",
+        defaultLanguage: "python"
+    });
     const graph = new Graph("graphContainer");
     let dataset = generateRandomArray(20);
     let generator = mergeSortInPlace(dataset);
