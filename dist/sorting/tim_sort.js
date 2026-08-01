3630647562752a03272b71efea02660679b2c0fe
diputs-sudo <diputs-sudo@proton.me>
2026-07-31 21:28:20 -0400

    update tim_sort.js

--- dist/sorting/tim_sort.js
+++ dist/sorting/tim_sort.js
@@ -21,6 +21,10 @@ document.addEventListener("DOMContentLoaded", () => {
         defaultLanguage: "python",
         moreInfoSelector: "#standardMoreInfo"
     });
+    initCodeLoader("sorting", "tim_sort_production", {
+        rootSelector: "#productionCodeSection",
+        defaultLanguage: "python"
+    });
     const graph = new Graph("graphContainer");
     let dataset = generateRandomArray(20);
     let generator = timSort(dataset);
