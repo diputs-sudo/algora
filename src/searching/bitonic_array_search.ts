6a0557a47ca121022c6ec33b977a26cca0d0197b
diputs-sudo <diputs-sudo@proton.me>
2026-08-01 16:38:31 -0400

    update bitonic_array_search.ts

--- src/searching/bitonic_array_search.ts
+++ src/searching/bitonic_array_search.ts
@@ -49,6 +49,10 @@ document.addEventListener("DOMContentLoaded", () => {
         defaultLanguage: "python",
         moreInfoSelector: "#standardMoreInfo"
     });
+    initCodeLoader("array_search", "bitonic_array_search_production", {
+        rootSelector: "#productionCodeSection",
+        defaultLanguage: "python"
+    });
 
     const visualizer = new SearchVisualizer("searchGraphContainer");
 
