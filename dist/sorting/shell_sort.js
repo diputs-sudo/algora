5e255ab2aa6e2edf5fb51f368cfe36caeac07345
diputs-sudo <diputs-sudo@proton.me>
2026-07-31 20:39:20 -0400

    update shell_sort.js

--- dist/sorting/shell_sort.js
+++ dist/sorting/shell_sort.js
@@ -11,6 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
         defaultLanguage: "python",
         moreInfoSelector: "#standardMoreInfo"
     });
+    initCodeLoader("sorting", "shell_sort_production", {
+        rootSelector: "#productionCodeSection",
+        defaultLanguage: "python"
+    });
     const graph = new Graph("graphContainer");
     let dataset = generateRandomArray(20);
     let generator = shellSort(dataset);
