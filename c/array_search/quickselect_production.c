ba768b27a276de88f5d617a4008f9e6ff8e24daa
diputs-sudo <diputs-sudo@proton.me>
2026-08-10 13:47:24 -0400

    add quickselect_production.c

---
+++ c/array_search/quickselect_production.c
@@ -0,0 +1,55 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+static void swap_int(int *left, int *right) {
+    int temp = *left;
+    *left = *right;
+    *right = temp;
+}
+
+static size_t partition(int *values, size_t left, size_t right, size_t pivot_index) {
+    int pivot = values[pivot_index];
+    swap_int(&values[pivot_index], &values[right]);
+    size_t store = left;
+
+    for (size_t scan = left; scan < right; scan++) {
+        if (values[scan] < pivot) {
+            swap_int(&values[store], &values[scan]);
+            store++;
+        }
+    }
+
+    swap_int(&values[store], &values[right]);
+    return store;
+}
+
+bool quickselect(int *values, size_t length, size_t k, int *result) {
+    if (values == NULL || result == NULL || length == 0 || k == 0 || k > length) {
+        return false;
+    }
+
+    size_t left = 0;
+    size_t right = length - 1;
+    size_t target = k - 1;
+
+    while (left <= right) {
+        size_t pivot_index = left + (size_t)(rand() % (int)(right - left + 1));
+        pivot_index = partition(values, left, right, pivot_index);
+
+        if (pivot_index == target) {
+            *result = values[pivot_index];
+            return true;
+        }
+        if (pivot_index > target) {
+            if (pivot_index == 0) {
+                return false;
+            }
+            right = pivot_index - 1;
+        } else {
+            left = pivot_index + 1;
+        }
+    }
+
+    return false;
+}
